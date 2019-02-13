e59938a33757059375cbf6485ae2017348e565f0
shubo7996 <whatsupmofo420@gmail.com>
2019-02-13 10:04:04 +0530

    --done--

--- order.py
+++ order.py
@@ -3,17 +3,16 @@
 
 """
 from random import randint
+from collections import Counter
 
-dict={}
+
 class Order(object):
 
 	def __init__(self,g_List=None,value=None):
 		self.__groupList=[]
-		self.group_list =g_List
-
+		self.group_list =g_List #group_list being a property returns a list 
 		self.__value=value
 		
-
 	@property
 	def group_list(self):
 		return self.__groupList
@@ -21,10 +20,13 @@ class Order(object):
 	@group_list.setter
 	def group_list(self,list_val):
 		if list_val is None:
+			print("first scenario")
 			for _ in range(10):
-				self.__groupList.append(randint(1,15))
-		elif isinstance(list_val,list):
-			self.__groupList=list_val
+				#print("adding!")
+				self.__groupList.append(randint(1,10))
+		#elif isinstance(list_val,list):
+		#	print("last case")
+		#	self.__groupList=list_val
 
 
 	@property
@@ -37,26 +39,42 @@ class Order(object):
 		self.__value=int(val)
 
 	def computePower(self):
-		global dict
-		length=len(self.group_list)
-		i=1
-		while i>1:
-			dict[i]=dict[i-1]^self.number
-			tempVar=dict[i] % length 
-			if tempVar==self.number:
-				print("The group is cyclic!")
-				print("Order of The number {} is {}".format(self.number,i))
-				return			
-			i+=1
+		listValue=[]
+		dict={}
+		groupList=self.group_list
+		value=self.number
+		length=len(groupList)
+		c=0
+		while True:
+			for i in range(1,30):
+				if dict.items() is None:
+					dict[i]=value
+				else:
+					dict[i]=(value**i)%(length+1)
+			break
+		print(dict)
+		for val in dict.values():
+			if val==value:
+				c+=1
+			else:
+				#print("Will continue!")
+				continue
+			if c==2:
+				#print(val)
+				max_key=list(dict.keys())[list(dict.values()).index(dict[i])]+1
+				print('Order of {} is: {}'.format(value,max_key))
+				print("Its cyclic!")
+				break
 
+		
 def main():
 	print("Example with random list")
 	obj = Order()
 	print("List of numbers: {}".format(obj.group_list))
-	#print("User selected: {}".format(obj.number))
 	obj.ask_user_for_number()
-	in_list = obj.number in obj.group_list
-	print(in_list)
+	obj.computePower()
+	#in_list = obj.number in obj.group_list
+	#print(in_list)
 
 if __name__ == '__main__':
 	main()
